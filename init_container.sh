40352f4b8c2cda5e8bb61d885c6dc3f51e01d300
Another Redbeard <another.redman@gmail.com>
2025-05-05 17:05:11 -0500

    chore: Clean up Dockerfile and fix working directory case sensitivity; enhance init_container.sh to print environment variables

--- init_container.sh
+++ init_container.sh
@@ -21,4 +21,9 @@ service ssh start
 # Get environment variables to show up in SSH session
 eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
 
+# Add this section to print environment variables during startup
+echo "--- Printing Environment Variables ---"
+printenv
+echo "--- End Environment Variables ---"
+
 /opt/startup.sh "$@"

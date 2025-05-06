de64e4c6eb8c3a6c901fec738c81382a55fd5018
Another Redbeard <another.redman@gmail.com>
2025-05-06 15:13:48 -0500

    fix: Update Dockerfile and startup.sh to correct WebJob directory paths

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+# Copy webjob files to the wwwroot folder
+mkdir -p /home/site/wwwroot/App_Data/jobs/triggered/TriggeredDemo
+cp /webjobs/triggered/triggeredDemo/* /home/site/wwwroot/App_Data/jobs/triggered/TriggeredDemo
+
 echo "Working folder: $(pwd)"
 echo "defaulting to command: \"dotnet dotnetcoresample.dll\""
 dotnet dotnetcoresample.dll

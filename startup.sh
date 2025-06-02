901df252a7c9725df26deb7ad327a3add604f780
Another Redbeard <another.redman@gmail.com>
2025-06-02 15:29:47 -0500

    fix: Update Dockerfile and startup script to streamline webjob file copying

--- startup.sh
+++ startup.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Copy webjob files to the wwwroot folder
-mkdir -p /home/site/wwwroot/App_Data/jobs/triggered/TriggeredDemo
-cp /webjobs/triggered/triggeredDemo/* /home/site/wwwroot/App_Data/jobs/triggered/TriggeredDemo
+#mkdir -p /home/site/wwwroot/App_Data/jobs/triggered/TriggeredDemo
+#cp /webjobs/triggered/triggeredDemo/* /home/site/wwwroot/App_Data/jobs/triggered/TriggeredDemo
 
 echo "Working folder: $(pwd)"
 echo "defaulting to command: \"dotnet dotnetcoresample.dll\""
